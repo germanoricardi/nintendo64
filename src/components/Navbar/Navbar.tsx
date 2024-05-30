aeac53192d39c06997ce6141799f8285cdd895fe
Germano Ricardi <germanoricardi7@gmail.com>
2024-05-30 13:47:05 -0400

    Added cn heper;

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,42 +5,53 @@ import {
   RouteIcon,
   UserIcon,
 } from "@/components";
+import { cn } from "@/helpers/cn";
 
-export const Navbar = () => {
-  const NavbarList = ({
-    children,
-    className,
-  }: {
-    className?: string;
-    children: React.ReactNode;
-  }) => {
-    return (
-      <ul
-        className={`my-4 border-t border-indigo-400/20 hover:border-indigo-400/40 ${className}`}
-      >
-        {children}
-      </ul>
-    );
-  };
+type NavbarProps = React.ComponentProps<"nav">;
+type NavbarListProps = React.ComponentProps<"ul">;
+type NavbarLisItemtProps = React.ComponentProps<"li">;
 
-  const NavbarListItem = ({
-    children,
-    className,
-  }: {
-    className?: string;
-    children: React.ReactNode;
-  }) => {
-    return (
-      <li
-        className={`my-2 p-2 rounded-lg bg-transparent hover:bg-indigo-400/40 hover:text-slate-100 cursor-pointer flex gap-2 items-center ${className}`}
-      >
-        {children}
-      </li>
-    );
-  };
-
+const NavbarList = ({ children, className, ...props }: NavbarListProps) => {
   return (
-    <nav className="flex flex-col h-screen bg-slate-900 border-r border-indigo-400/20 hover:border-indigo-400/40 w-72 p-2 text-slate-300">
+    <ul
+      className={cn(
+        "my-4 border-t border-indigo-400/20 hover:border-indigo-400/40",
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </ul>
+  );
+};
+
+const NavbarListItem = ({
+  children,
+  className,
+  ...props
+}: NavbarLisItemtProps) => {
+  return (
+    <li
+      className={cn(
+        "my-2 p-2 rounded-lg bg-transparent hover:bg-indigo-400/40 hover:text-slate-100 cursor-pointer flex gap-2 items-center",
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </li>
+  );
+};
+
+export const Navbar = ({ className, ...props }: NavbarProps) => {
+  return (
+    <nav
+      className={cn(
+        "flex flex-col h-screen bg-slate-900 border-r border-indigo-400/20 hover:border-indigo-400/40 w-72 p-2 text-slate-300",
+        className
+      )}
+      {...props}
+    >
       <div className="mt-2 flex items-center justify-center">
         <img
           src="https://germanoricardi.com.br/wp-content/uploads/2020/04/Logomarca-Germano-Ricardi-Claro.png"
