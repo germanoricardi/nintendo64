61bbdb2cb0c0c04a141db93684d24c1937dee8e4
Germano Ricardi <germanoricardi7@gmail.com>
2024-05-31 05:52:46 -0400

    Added custom color slate-890;

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        'slate-890': 'rgb(var(--color-slate-890) / <alpha-value>)'
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
