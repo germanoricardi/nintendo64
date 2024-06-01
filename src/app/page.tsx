867ecc481d99ee860e41afe83ce7538dca84e7ee
Germano Ricardi <germanoricardi7@gmail.com>
2024-05-31 20:36:58 -0400

    Added placeholder articles with images;

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 
+import articles from '@/data/articles.json'
+
 export default function Home() {
   return <div className="ml-72">
     <div className="w-full h-[35vh] bg-orange-400 flex-center">
@@ -8,10 +10,24 @@ export default function Home() {
 
     <div className="container mx-auto my-6">
       <div className="grid grid-cols-4 gap-4 h-[35vh]">
-        <div className="flex-center bg-indigo-500">01</div>
-        <div className="flex-center bg-indigo-600">02</div>
-        <div className="flex-center bg-indigo-700">03</div>
-        <div className="flex-center bg-indigo-800">04</div>
+        {
+          articles.splice(-4).map(article => {
+            return(
+              <div key={article.title} className="flex-center relative overflow-hidden">
+                <div className="h-full w-full">
+                  <Image 
+                    className="h-full w-full object-cover transition duration-500 hover:scale-105 rounded-r-lg"
+                    src={`/assets/images/articles/${article.image}`}
+                    alt={article.title}
+                    width={600}
+                    height={400}
+                  />
+                </div>
+                  <p className="absolute bottom-0 pt-6 pb-2 px-2 bg-gradient-to-t from-slate-900 via-slate-800 to-transparent w-full">{article.title}</p>
+              </div>
+            )
+          })
+        }
       </div>
     </div>
 
@@ -19,33 +35,33 @@ export default function Home() {
       <div className="grid grid-cols-12 gap-4">
         
         <div className="col-span-8 flex flex-col gap-4">
-          {/* Item */}
-          <div className="flex gap-4 bg-purple-500 rounded-md">
-            <div>
-              <img src="https://placehold.co/600x400" alt="Descrição" className="w-auto h-[200px]" />
-            </div>
-            
-            <div className="flex flex-col gap-2 py-4">
-              <h2 className="text-3xl">Título aqui</h2>
-              <p className="flex-grow">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-              <button>Ler mais</button>
-            </div>
-          </div>
-          {/* /Item */}
-
-          {/* Item */}
-          <div className="flex gap-4 bg-purple-500 rounded-md">
-            <div>
-              <img src="https://placehold.co/600x400" alt="Descrição" className="w-auto h-[200px]" />
-            </div>
-            
-            <div className="flex flex-col gap-2 py-4">
-              <h2 className="text-3xl">Título aqui</h2>
-              <p className="flex-grow">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-              <button>Ler mais</button>
-            </div>
-          </div>
-          {/* /Item */}
+          {
+            articles.map(article => {
+              return (
+                // <Item>
+                <div key={article.title} className="flex bg-slate-800 rounded-md py-4">
+                  <div className="flex items-center">
+                    <div className="h-40 rounded-r-lg overflow-hidden">
+                      <Image 
+                        className="h-full w-full object-cover transition duration-500 hover:scale-105 rounded-r-lg"
+                        src={`/assets/images/articles/${article.image}`}
+                        alt={article.title}
+                        width={600}
+                        height={400}
+                      />
+                    </div>
+                  </div>
+                  
+                  <div className="w-full flex flex-col gap-2 pl-4">
+                    <h2 className="text-2xl text-indigo-400 mb-4">{article.title}</h2>
+                    <p className="flex-grow">{article.excerpt}</p>
+                    <button className="bg-slate-700 hover:bg-indigo-400/40 rounded-lg px-4 py-2 inline max-w-max">Ler mais</button>
+                  </div>
+                </div>
+                // </Item>
+              )
+            })
+          }
         </div>
 
         <div className="col-span-4 bg-indigo-700 rounded-md flex-center">banner</div>
